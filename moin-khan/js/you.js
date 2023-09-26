80ad8ddd1609fae729e533c8a0dce2b25102c743
Moin Khan <moin7657467@gmail.com>
2023-09-26 17:34:19 +0600

    first commit

---
+++ moin-khan/js/you.js
@@ -0,0 +1,33 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+// function firstPageAnim() {
+//     var tl = gsap.timeline();
+
+//     tl.from("#nav", {
+//         y: '-10',
+//         opacity: 0,
+//         duration:1.5,
+//         ease:Expo.easeInOut
+//     })
+//     .to(".boundingelem",{
+//         ease:Expo.easeInOut,
+//         duration: 2,
+//         Stagger: .2
+//     })
+// }
+
+
+
+
+// function circleMouseFollo() {
+//     window.addEventListener("mousemove", function(dets) {
+//         document.querySelector("mincircle").style.transform = `translate(${dets.clientX})px,${dets.clientY}px`;
+//     })
+// }
+
+// circleMouseFollo();
+
+
